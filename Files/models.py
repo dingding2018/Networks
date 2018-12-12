dca39d14a9bee91ec634fac138bc8f28aa5fef75
dingding <yd1300@nyu.edu>
2018-12-12 11:22:57 -0500

    db model related files updated

--- Files/models.py
+++ Files/models.py
@@ -28,9 +28,20 @@ class Comment(db.Model):
     __tablename__ = 'comment'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     content = db.Column(db.Text, nullable=False)
-    #create_time = db.Column(db.DateTime, default=datetime.now)
+    create_time = db.Column(db.DateTime, default=datetime.now)
     question_id = db.Column(db.Integer, db.ForeignKey('question.id'))
     author_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
     question = db.relationship('Question', backref=db.backref('comments',order_by=id.desc()))
     author = db.relationship('User', backref=db.backref('comments'))
+
+class Underwrite(db.Model):
+    __tablename__ = 'underwrite'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.Text, nullable=False)
+    gender = db.Column(db.String, nullable=False)
+    age = db.Column(db.Integer, nullable=False)
+    occupation = db.Column(db.String, nullable=False)
+    author_id = db.Column(db.Integer, db.ForeignKey('user.id')) 
+
+    author = db.relationship('User', backref=db.backref('underwrite'))
